cfce2042fcd83c314593879b054a6172c57288fe
priscillaCN <priscillannamani00@gmail.com>
2026-05-27 07:51:20 +0100

    Book Suggestion System complted in java only

---
+++ DayTwo/java/BookSuggestionSystem.java
@@ -0,0 +1,109 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class BookSuggestionSystem {
+
+    public static String displayMenu(String menu) {
+    
+       menu = """
+            1. Get Suggestions
+            2. Add Book
+            3. Remove Book
+            4. Update Book
+            5. Show All Books
+            """;
+
+        return menu;
+
+    }
+
+    public static String getBookSuggestion (ArrayList <String> books, Random bookSuggestion) {
+    
+        int randomIndex = 0;
+        String selectedBook = "";
+
+        if(books.size() == 0) {
+            throw new IllegalArgumentException("Your library is empty. please add books");
+        }
+
+        else {
+            for(int index = 0; index < books.size(); index++) {
+                randomIndex = bookSuggestion.nextInt(books.size());
+            }
+
+            selectedBook = books.get(randomIndex);
+        }
+
+        return selectedBook;
+    }
+
+
+    public static boolean isPageSuggestionWithinRange (Random pageSuggestion) {
+
+        int randomPageNumber = pageSuggestion.nextInt(1, 101);
+
+        if(randomPageNumber > 0 && randomPageNumber <= 100) return true;
+
+        return false;
+
+    }
+
+
+    public static String addBook (String newBook, ArrayList <String> books) {
+        
+        for(String book : books) {
+            if(book.equalsIgnoreCase(newBook)) {
+                throw new IllegalArgumentException("Book already exists");
+            }
+        }             
+
+        books.add(newBook);
+        return "Book added successfully"; 
+     
+    }
+
+
+    public static String removeBook (String oldBook, ArrayList <String> books) {
+
+        for(String book : books) {
+            if(book.equalsIgnoreCase(oldBook)) {
+                books.remove(oldBook);
+                return "Book removed successfully";      
+            }
+        }
+  
+        throw new IllegalArgumentException("Book not found");
+
+    }
+
+
+    public static String updateBook (String oldTitle, String newTitle, ArrayList <String> books) {
+
+        for(String book : books) {
+            if(!book.equalsIgnoreCase(oldTitle)) {
+                throw new IllegalArgumentException("Book not found");
+            }
+        }             
+        for(String book : books) {
+            if(book.equalsIgnoreCase(oldTitle)) {
+                books.remove(book);
+                books.add(newTitle);
+            }
+        } 
+ 
+        return "Book updated successfully"; 
+     
+    }
+
+
+    public static void showBooks(ArrayList <String> books) {
+
+        for (String book : books) {
+            System.out.println(book);
+        }
+
+    }
+    
+
+}
---
+++ DayTwo/java/BookSuggestionSystemTest.java
@@ -0,0 +1,211 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+
+
+public class BookSuggestionSystemTest {
+
+    ArrayList<String> books;
+
+    @BeforeEach
+    void setUp(){
+       books  = new ArrayList<>();
+    }
+
+    @Test
+    public void givenDisplayMenu_whenInputIsMenu_thenDisplaySystemMenu() {
+    
+        String menu = """
+            1. Get Suggestions
+            2. Add Book
+            3. Remove Book
+            4. Update Book
+            5. Show All Books
+            """;
+
+    assertEquals(menu, BookSuggestionSystem.displayMenu(menu));
+
+    }
+
+
+    @Test
+    public void givenGetBookSuggestion_whenInputIsAnEmptyArrayList_thenThrowAnIllegalArgumentException() {
+    
+        Random testRandom =  new Random();
+
+        assertThrows(IllegalArgumentException.class, () -> BookSuggestionSystem.getBookSuggestion(books, testRandom));
+
+    }
+
+
+    @Test
+    public void givenGetBookSuggestion_whenInputIsAnArrayListWithBooks_thenReturnRandomlySuggestedBook() {
+   
+
+        books.add("The Client");
+        books.add("Gone Girl");
+        books.add("Purple Hibiscus");
+        books.add("Blink Of An Eye");
+
+        String selectedBook = "Gone Girl";
+        int randomIndex = books.indexOf(selectedBook);
+        Random testRandom =  new Random(randomIndex);
+    
+        assertEquals(selectedBook, BookSuggestionSystem.getBookSuggestion(books, testRandom));
+
+        selectedBook = "Blink Of An Eye";
+        randomIndex = books.indexOf(selectedBook);
+        testRandom =  new Random(randomIndex);
+    
+        assertEquals(selectedBook, BookSuggestionSystem.getBookSuggestion(books, testRandom));
+
+    }
+
+
+
+    @Test
+    public void givenIsPageSuggestionWithinRange_whenInputIsRandomWithValue100_thenReturnTrue() {
+
+        Random testRandom =  new Random();
+        boolean result = BookSuggestionSystem.isPageSuggestionWithinRange(testRandom);
+
+        assertTrue(result);
+
+    }
+
+
+    @Test
+    public void givenAddBook_whenInputIsOneBook_thenReturnBookAddedMessage() {
+
+        String title = "Davinci Code";
+        String expected = "Book added successfully";
+
+        assertEquals(expected, BookSuggestionSystem.addBook(title, books));
+
+    }
+
+
+    @Test
+    public void givenAddBook_whenInputIsABookThatAlreadyExists_thenThrowAnIllegalArgumentException() {
+
+        books = new ArrayList<>(Arrays.asList("Life At Semicolon"));
+        String title = "Life At Semicolon";
+
+        assertThrows(IllegalArgumentException.class, () -> BookSuggestionSystem.addBook(title, books));
+
+    }
+
+
+    @Test
+    public void givenAddBook_whenInputIsABookThatAlreadyExistsWrittenInUppercase_thenThrowAnIllegalArgumentException() {
+
+        books = new ArrayList<>(Arrays.asList("Life At Semicolon"));
+        String title = "LIFE AT SEMICOLON";
+
+        assertThrows(IllegalArgumentException.class, () -> BookSuggestionSystem.addBook(title, books));
+
+    }
+
+
+    @Test
+    public void givenAddBook_whenInputIsABookThatAlreadyExistsWrittenInLowercase_thenThrowAnIllegalArgumentException() {
+
+        books = new ArrayList<>(Arrays.asList("Life At Semicolon"));
+        String title = "life at semicolon";
+
+        assertThrows(IllegalArgumentException.class, () -> BookSuggestionSystem.addBook(title, books));
+
+    }
+
+
+    @Test
+    public void givenRemoveBook_whenInputIsABookThatAlreadyExists_thenReturnBookRemovedMessage() {
+
+        books = new ArrayList<>(Arrays.asList("Life At Semicolon"));
+        String title = "Life At Semicolon";
+        String expected = "Book removed successfully";
+
+        assertEquals(expected, BookSuggestionSystem.removeBook(title, books));
+
+
+    }
+
+
+    @Test
+    public void givenRemoveBook_whenInputIsABookThatAlreadyExistsWrittenInUppercase_thenReturnBookRemovedMessage() {
+
+        books = new ArrayList<>(Arrays.asList("Life At Semicolon"));
+        String title = "LIFE AT SEMICOLON";
+        String expected = "Book removed successfully";
+
+        assertEquals(expected, BookSuggestionSystem.removeBook(title, books));
+
+
+    }
+
+
+    @Test
+    public void givenRemoveBook_whenInputIsABookThatAlreadyExistsWrittenInLowercase_thenReturnBookRemovedMessage() {
+
+        books = new ArrayList<>(Arrays.asList("Life At Semicolon"));
+        String title = "life at semicolon";
+        String expected = "Book removed successfully";
+
+        assertEquals(expected, BookSuggestionSystem.removeBook(title, books));
+
+
+    }
+
+
+    @Test
+    public void givenRemoveBook_whenInputIsABookThatDoesNotExists_thenThrowAnIllegalArgumentException() {
+
+        books = new ArrayList<>(Arrays.asList("Life At Semicolon"));
+        String title = "Davinci Code";
+
+        assertThrows(IllegalArgumentException.class, () -> BookSuggestionSystem.removeBook(title, books));
+
+    }
+
+
+    @Test
+    public void givenUpdateBook_whenOldTitleIsUpdated_thenReturnBookUpdatedMessage() {
+
+        books = new ArrayList<>(Arrays.asList("Life At Semicolon"));
+        String oldTitle = "Life At Semicolon";
+        String newTitle = "No Life At Semicolon";
+        String expected = "Book updated successfully";
+
+        assertEquals(expected, BookSuggestionSystem.updateBook(oldTitle, newTitle, books));
+
+
+    }
+
+
+    @Test
+    public void givenUpdateBook_whenOldTitleDoesNotExists_thenThrowAnIllegalArgumentException() {
+
+        books = new ArrayList<>(Arrays.asList("Life At Semicolon"));
+        String oldTitle = "Davinci Code";
+        String newTitle = "";
+
+        assertThrows(IllegalArgumentException.class, () -> BookSuggestionSystem.updateBook(oldTitle, newTitle, books));
+
+    }
+
+    @Test
+    public void givenShowBooks_whenInputIsAnArrayListWithBooks_thenDisplayListOfBooks() {
+
+        books = new ArrayList<>(Arrays.asList("Life At Semicolon", "The Client", "Purple Hibiscus", "Blink Of An Eye"));
+        BookSuggestionSystem.showBooks(books);
+
+    }
+    
+
+}
